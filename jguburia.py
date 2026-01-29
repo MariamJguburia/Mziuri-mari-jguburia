c83bc181de6e80da00654178ab416ab973e6bfe6
lenovo <mjguburia483@gmail.com>
2026-01-29 15:04:53 +0400

    to github

---
+++ jguburia.py
@@ -0,0 +1,34 @@
+with open('file.txt','r') as files:
+    count = []
+    for i in files:
+        number = int(i.strip())
+        count.append(number**2)
+    print(count)
+
+with open('squared numbers.txt', 'w') as output:
+    for num in count:
+        output.write(str(num) + '\n')
+
+
+#2
+
+
+age = []
+name = []
+with open('oscar.txt', 'r') as file:
+    for line in file:
+        parts = line.split(',')
+        age.append(int(parts[2]))
+        name.append(parts[3] + ' ' + parts[4])
+
+minimum_age = min(age)
+index = age.index(minimum_age)
+
+print('\n','name, surname:', name[index])
+print('age:', minimum_age)
+
+with open('oscar.txt', 'r') as oscar_file:
+    year= input('Year: ')
+    for char in oscar_file:
+        if year in char:
+            print(char.strip())
